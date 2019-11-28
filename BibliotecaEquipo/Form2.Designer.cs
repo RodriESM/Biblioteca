6817c695c5d12b58e6ac4b1b7f4ee883539e3f9b
RodriESM <58076227+RodriESM@users.noreply.github.com>
2019-11-28 09:54:21 +0100

    Form 2 Label actualizado

--- BibliotecaEquipo/Form2.Designer.cs
+++ BibliotecaEquipo/Form2.Designer.cs
@@ -35,40 +35,44 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(29, 127);
+            this.textBox1.Location = new System.Drawing.Point(22, 103);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(384, 62);
+            this.textBox1.Size = new System.Drawing.Size(289, 51);
             this.textBox1.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(148, 74);
+            this.label1.Location = new System.Drawing.Point(18, 67);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(241, 29);
+            this.label1.Size = new System.Drawing.Size(192, 24);
             this.label1.TabIndex = 3;
             this.label1.Text = "Nombre de Usuario";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(123, 207);
+            this.button1.Location = new System.Drawing.Point(92, 168);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(165, 62);
+            this.button1.Size = new System.Drawing.Size(124, 50);
             this.button1.TabIndex = 4;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(433, 295);
+            this.ClientSize = new System.Drawing.Size(325, 240);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form2";
             this.Text = "Form2";
             this.Load += new System.EventHandler(this.Form2_Load);
